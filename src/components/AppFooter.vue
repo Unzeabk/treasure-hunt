87e527ba04d7260685dc9c276573943488080779
Lev <lev@Levs-MacBook-Air.local>
2022-09-05 23:17:06 +0300

    Footer link change

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,7 +2,7 @@
   <div class="footer-proof">
     <p>
       You can check out the source code
-      <a href="https://github.com/Unzeabk">here</a>.
+      <a href="https://github.com/Unzeabk/treasure-hunt">here</a>.
     </p>
   </div>
 </template>
